8a16982ab3020cf0a83be0fa7cc300fa1c6928e7
albertlorenzo <albertlorenzofc@gmail.com>
2019-05-10 23:04:37 +0200

    Code Wars

--- replace_with_alphabet_position.js
+++ replace_with_alphabet_position.js
@@ -1,5 +1,9 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+const alphabetPosition = (chain) => chain.toLowerCase().match(/[a-z]/gi).map((e, i, a) => alphabet.indexOf(e) + 1).join(' ')
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
+
 // Solución @hencethus sustituyendo el diccionario por charCodeAt - 64
-// La solución original utilizaba un alfabeto -> const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const alphabetPosition = (chain) => chain.toUpperCase().match(/[a-z]/gi).map(e => e.charCodeAt() - 64).join(' ')
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock."))
