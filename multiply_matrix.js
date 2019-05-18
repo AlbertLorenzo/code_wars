eb050789584505f10f13f76ec3ac1f6c88a95da8
albertlorenzo <albertlorenzofc@gmail.com>
2019-05-18 11:09:27 +0200

    code wars

--- multiply_matrix.js
+++ multiply_matrix.js
@@ -1,5 +1,5 @@
 const multiplyMatrix = (a, b) => {
-    const product = Array.from({ length: a.length }, (_, i) => Array({ length: b.length }, (_, k) => k).fill(0))
+    const product = Array.from({ length: a.length }, (_, i) => Array({ length: b[0].length }, (_, k) => k).fill(0))
     return product.map((vector, i) =>
         vector.map((value, j) =>
             a[i].reduce((total, n, k) => total + (n * b[k][j]), 0
@@ -7,13 +7,15 @@ const multiplyMatrix = (a, b) => {
 }
 
 const m1 = [
-    [1, 2],
-    [3, 4]
+    [1, 2, 3],
+    [3, 4, 5],
+    [3, 4, 5]
 ]
 
 const m2 = [
-    [5, 6],
-    [7, 8]
+    [1, 2, 3],
+    [4, 5, 6],
+    [1, 2, 3]
 ]
 
 console.log(multiplyMatrix(m1, m2))
