c3239d3bdc9dfa394cdb813a9a896e2322f9ec0d
Albert Lorenzo Segarra <albertlorenzofc@gmail.com>
2019-06-17 12:51:55 +0200

    Update is_isogram.js

--- is_isogram.js
+++ is_isogram.js
@@ -2,8 +2,4 @@ const isogram = (sortAlgorithm) => (word) => sortAlgorithm(word)
 
 const sortAlgorithm = (word) => !(word.toLowerCase().split('').some((e, i, a) => a.indexOf(e) != i))
 
-const isIsogram = isogram(sortAlgorithm)
-
-console.log(isIsogram('isogram'))
-
-// Otra solución por @chunjef code wars return !/(\w).*\1/i.test(str)
+console.log(isogram(sortAlgorithm)('isogram'))
