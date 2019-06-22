e929692bf8d7294b793eccb22dc69bf23f08d160
Albert Lorenzo Segarra <albertlorenzofc@gmail.com>
2019-06-22 14:57:06 +0200

    Update sort_by_firt_recurring_character.js

--- sort_by_firt_recurring_character.js
+++ sort_by_firt_recurring_character.js
@@ -2,11 +2,8 @@ const vector = 'DBKCSKA'.split('')
 
 const firstRecurring = (sort) => (v) => sort(v)
 
-// Con array.indexOf(elemento) != i 
-// Comprobamos de izquierda a derecha que una string
-// contenga un segundo valor <e> idéntico al que estamos buscando
 const sortAlgorithm = () => vector.filter((e, i, a) => (a.indexOf(e) != i)).map(e => e).join('')
 
-const result = firstRecurring(sortAlgorithm)
+const result = firstRecurring(sortAlgorithm)(vector)
 
-console.log(result(vector))
+console.log(result)
