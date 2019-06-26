ab4680ebc81c5b4f056880c70561731362a010fa
Albert Lorenzo Segarra <albertlorenzofc@gmail.com>
2019-06-26 17:17:31 +0200

    Update alphabet_war.js

--- alphabet_war.js
+++ alphabet_war.js
@@ -28,10 +28,3 @@ const alphabetWar = (str) => {
 }
 
 alphabetWar("rn")
-
-// // Solución de @Willworth code wars
-// function alphabetWar(fight) {
-//     const map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 }
-//     const result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0)
-//     return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!"
-// }
